093ed9a884e7b79222d31928517acf9f2fd576f2
jholle7470 <89286250+jholle7470@users.noreply.github.com>
2025-06-06 20:11:09 -0400

    Added ejs layout

--- index.js
+++ index.js
@@ -3,15 +3,38 @@
 // 1) Load Express:
 const express = require('express');
 
+// 1.5) Create the path variable
+const path = require('path');
+
 // 2) Create an Express app:
 const app = express();
+
+const expressLayouts = require('express-ejs-layouts');
+
+// Tell Express where to find static assets:
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set EJS as the view engine and specify the 'views' folder
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // 3) Define a port (use environment variable or fallback to 3000):
 const PORT = process.env.PORT || 3000;
 
+app.use(expressLayouts); //Wires up express-ejs-layouts inside this index.js
+
 // 4) Set up a route for GET "/" that responds with "Hello, World!"
 app.get('/', (req, res) => {
-  res.send('Hello, World Version 2 Express');
+  res.render('index');
+});
+
+// Example JSON API endpoint:
+app.get('/api/info', (req, res) => {
+  res.json({
+    app: 'node-hello-world',
+    version: '1.0.0',
+    author: 'Jeremy H'
+  });
 });
 
 // 5) Start listening on PORT:
