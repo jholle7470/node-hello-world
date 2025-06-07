afc722d9f71a2e2554d5a6a1c53c08c14a0582e6
jholle7470 <89286250+jholle7470@users.noreply.github.com>
2025-06-06 20:24:21 -0400

    Added Notes File

---
+++ git.cmd
@@ -0,0 +1,9 @@
+git add .  //adds all files and filled folders to the staging area
+git commit -m "Text of description" //Adds changes to the local repo
+git push  //pushes current local branch to remote repo i.e. github
+
+//empty folders will not be added to the remote repo
+
+//To add a file to a folder or anywhere you can use touch folderName/filename.ext
+//Use .gitkeep as a dummy file
+
